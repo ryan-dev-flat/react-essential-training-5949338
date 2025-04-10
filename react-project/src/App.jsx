4f6e6d454eaed850416971ad554979a8bd55bbf2
ryan-dev-flat <coap.ryanmontgomery@gmail.com>
2025-04-10 13:22:36 -0600

    commit first

---
+++ react-project/src/App.jsx
@@ -0,0 +1,46 @@
+import './App.css'
+
+function Header({ name, year }) {
+  
+  return (
+    <header>
+      <h1>{name}'s Kitchen</h1>
+      <p>Copyright {year}</p>
+    </header>
+  )
+}
+const items = [
+  "Macaroni and Cheese",
+  "Salmon with Potatoes",
+  "Tofu with Vegetables",
+  "Minestrone Soup"
+];
+
+const dishObjects = items.map((dish, i) => ({
+  id: i,
+  title: dish
+}))
+
+console.log(dishObjects)
+
+function Main({dishes}) {
+  return (
+  <ul>
+    {dishes.map((dish) => (
+      <li key={dish.id} style={{listStyleType: "none"}}>
+        {dish.title}</li> ))}
+  </ul>
+    );
+}
+
+function App() {
+  
+  return (
+    <div>
+      <Header name="Alex" year={new Date().getFullYear()}/>
+    <Main dishes={dishObjects}/>
+    </div>
+  );
+}
+
+export default App
